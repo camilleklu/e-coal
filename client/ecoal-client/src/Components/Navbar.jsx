132807ce1f0820875549f90f02b5d0110c5930b2
Mahieux Celestine <etampes2@gmail.com>
2025-03-06 13:47:12 +0100

    final of the nav bar

--- client/ecoal-client/src/Components/Navbar.jsx
+++ client/ecoal-client/src/Components/Navbar.jsx
@@ -15,12 +15,15 @@ const Navbar = () => {
   return (
 
     <nav className={styles.navbar}>
-
+      <div className={styles.navbar_logo_burger}>
       <div className="navbar-burger" onClick={toggleMenu}>
       <box-icon name='menu' className={styles.icon}></box-icon>
       </div>
       <h1 className={styles.logo}>K-Music Insider</h1>
+      </div>
       <ul className={`${styles['navbar-list']} ${menuOpen ? styles.show : ''}`}> 
+        <li className={styles['navbar-item']}><Link to="/" onClick={toggleMenu}>Home</Link></li>
+        <li className={styles['navbar-item']}><Link to="/articles" onClick={toggleMenu}>Articles</Link></li>
         <li className={styles['navbar-item']}><Link to="/search" onClick={toggleMenu}>Search</Link></li>
         <li className={styles['navbar-item']}><Link to="/login" onClick={toggleMenu}>Login</Link></li>
         <li className={styles['navbar-item']}><Link to="/register" onClick={toggleMenu}>Register</Link></li>
